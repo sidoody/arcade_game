4c9158b242017eb10ec920d99762a1dcfca1e5e1
sidoody <saudsidd@gmail.com>
2017-08-21 13:48:54 -0400

    added bug movement

--- js/app.js
+++ js/app.js
@@ -9,9 +9,12 @@ var Enemy = function(x, y, speed) { //this is an enemy..now below put what it ne
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  if (this.x > -200 && this.x < 500){
+    this.x += this.speed * dt;
+  }
+  else {
+    this.x = -100;
+  }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -33,30 +36,43 @@ Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
 Player.prototype.update = function() {
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [];
+var allEnemies = [new Enemy(-100, 210,100), new Enemy(-100, 130, 200), new Enemy(-100, 50, 300)];
 var player = new Player(200, 400, 100);
-var enemy = new Enemy(200,200,30);
+
+/*
+for collisions:
+
+I loop through the allEnemies array ,
+then I check each iteration if x and y = player x and y,
+if true then reset the player to init position
+
+will be done in player update
+because it updates the player
+*/
 
 player.handleInput = function(key) {
-    if (key === 'left') {
+    if (key === 'left' && player.x > 0) {
         player.x -= player.speed;
     }
-    if (key === 'up') {
+    if (key === 'up' && player.y > 0) {
         player.y -= player.speed;
     }
-    if (key === 'right') {
+    if (key === 'right' && player.x < 400) {
         player.x += player.speed;
     }
-    if (key === 'down') {
+    if (key === 'down' && player.y < 400) {
         player.y += player.speed;
     }
 };
+
+
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -70,12 +86,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
-/*for later:
-
-y == -20 == in the water
-x == 0 == left wall
-x == 400 == right wall
-y == 400 == bottom wall
-*/
